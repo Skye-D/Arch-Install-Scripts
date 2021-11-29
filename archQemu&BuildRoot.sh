9e1169e85506feed89189d9d97b8e5ddaa0ef708
SkyeNet <71424716+Skye-D@users.noreply.github.com>
2021-11-29 10:58:34 -0500

    Update archQemu&BuildRoot.sh

--- archQemu&BuildRoot.sh
+++ archQemu&BuildRoot.sh
@@ -1,8 +1,10 @@
 #!bin/bash
 wget https://buildroot.org/downloads/buildroot-2021.02.7.tar.bz2
 tar -xf buildroot-2021.02.7.tar.bz2
+rm buildroot-2021.02.7.tar.bz2
 wget https://download.qemu.org/qemu-6.2.0-rc2.tar.xz
 tar xvJf qemu-6.2.0-rc2.tar.xz
+rm qemu-6.2.0-rc2.tar.xz
 cd qemu-6.2.0-rc2
 sudo pacman -S ninja -y
 ./configure
