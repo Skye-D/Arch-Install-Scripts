b334e0841664ba14d109e1eeec46c8feb943e9f9
SkyeNet <71424716+Skye-D@users.noreply.github.com>
2021-11-29 02:39:43 -0500

    Update and rename archQemu.sh to archQemu&BuildRoot.sh

--- archQemu&BuildRoot.sh
+++ archQemu&BuildRoot.sh
@@ -1,3 +1,5 @@
+wget https://buildroot.org/downloads/buildroot-2021.02.7.tar.bz2
+tar -xf buildroot-2021.02.7.tar.bz2
 wget https://download.qemu.org/qemu-6.2.0-rc2.tar.xz
 tar xvJf qemu-6.2.0-rc2.tar.xz
 cd qemu-6.2.0-rc2
